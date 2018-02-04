159bb7e27ac248826fb6ea42f52ee3534ca4cc9e
rnavis <ranjith.sivanand@gmail.com>
2018-02-04 17:55:37 -0500

    Custom bubble sort and Binary Search

---
+++ java/src/com/rnavis/basics/ogla/Hcraes.java
@@ -0,0 +1,36 @@
+package com.rnavis.basics.ogla;
+
+import java.util.Arrays;
+
+public class Hcraes {
+
+    public static void main(String args[]) {
+        BinarySearch bs = new BinarySearch();
+        bs.search(1);
+    }
+}
+class BinarySearch{
+    int[] array = {8, 4,13,9,11, 2, 15,7,5,21,3};
+    void search(int value) {
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+
+        int start =0;
+        int end = array.length-1;
+        while (start<=end) {
+            int mid  = (start + end)/ 2;
+            if (value == array[mid]){
+                System.out.println("found matching "+ value);
+                break;
+            }
+            if (value<array[mid]) {
+                end = mid-1;
+            } else {
+                start = mid+1;
+            }
+        }
+        if (start>end) {
+            System.out.println("no match found");
+        }
+    }
+}
---
+++ java/src/com/rnavis/basics/ogla/Retros.java
@@ -0,0 +1,35 @@
+package com.rnavis.basics.ogla;
+
+import java.util.Arrays;
+
+public class Retros {
+    public static void main(String args[]) {
+        BubbleUp bubbleUp = new BubbleUp();
+        System.out.println(Arrays.toString(bubbleUp.array));
+        System.out.println("after sort...");
+        bubbleUp.sort();
+        System.out.println(Arrays.toString(bubbleUp.array));
+    }
+}
+
+/**
+ * #1 loop through all elements in the array
+ * #2 for every element, loop back again
+ * #3 if the element > next element, then switch using a temp
+ * #4 in place sort
+ */
+class BubbleUp {
+    int[] array = {9,5,2,8,3,9,1,4,7};
+    void sort() {
+        int temp=0;
+        for (int i=0; i<array.length-1;i++) {
+            for (int j=0; j<array.length-1-i;j++) {
+                temp = array[j];
+                if (array[j] > array[j+1]) {
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+    }
+}
